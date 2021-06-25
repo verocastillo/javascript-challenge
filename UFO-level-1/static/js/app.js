e5043663b1d3098ef35a0497ff4030a16197eed3
Vero Castillo <77795761+verocastillo@users.noreply.github.com>
2021-06-24 20:55:31 -0500

    Level 1 works.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,52 @@
-// from data.js
+// Import dataset from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Select buttons
+var filterbutton = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#datetime")
+
+// Event handlers
+filterbutton.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Get reference to table 
+var tbody = d3.select("tbody");
+
+// Append data to table for initial page
+data.forEach((sighting) => {
+    var row = tbody.append("tr");
+    Object.values(sighting).forEach((value) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+});
+  
+// Complete the event handler function for the form
+function runEnter() {
+
+    // Select table and create reference to table
+    var htmlTable = d3.select("#ufo-table");
+    var tbody = d3.select("tbody")
+    tbody.html("");
+
+    // Select the input element
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var datetimeSearchCriteria = inputElement.property("value")
+    
+    // Function for filtering data
+    var filteredData = tableData.filter(sighting =>
+        sighting.datetime === datetimeSearchCriteria);
+    
+    // Results and append
+    filteredData.forEach((sighting) => {
+        var row = tbody.append("tr");
+        Object.values(sighting).forEach((value) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+    });
+}
